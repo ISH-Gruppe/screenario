8b081590f3963397517f2e3813f526fc1ae7404e
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-04 14:27:17 +0200

    feat: Add Gridlayout

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import CountdownTimer from "./components/CountdownTimer";
 import { Rnd } from "react-rnd";
 
-// import "/node_modules/react-grid-layout/css/styles.css";
-// import "/node_modules/react-resizable/css/styles.css";
-// import GridLayout from "react-grid-layout";
+import "/node_modules/react-grid-layout/css/styles.css";
+import "/node_modules/react-resizable/css/styles.css";
+import GridLayout from "react-grid-layout";
 
 // import "./digitalerstuhlkreis/runtime-es2015";
 // import "./digitalerstuhlkreis/runtime-es5";
@@ -20,8 +20,6 @@ import "./digitalerstuhlkreis_prod/runtime-es2015.a4dadbc03350107420a4";
 import "./digitalerstuhlkreis_prod/runtime-es5.a4dadbc03350107420a4";
 import "./digitalerstuhlkreis_prod/polyfills-es2015.d9c8f3d13d7372708bac";
 import "./digitalerstuhlkreis_prod/polyfills-es5.dab0688ff17f3e0c4dfd";
-// import "./digitalerstuhlkreis_prod/vendor-es2015";
-// import "./digitalerstuhlkreis_prod/vendor-es5";
 import "./digitalerstuhlkreis_prod/main-es2015.135e038542eda557c68f";
 import "./digitalerstuhlkreis_prod/main-es5.135e038542eda557c68f";
 import "./digitalerstuhlkreis_prod/styles.00f6a897faa5361a9828.css";
@@ -32,33 +30,59 @@ export default function App() {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
 
-  return (
-    <div>
-      <Rnd
-        default={{
-          x: 0,
-          y: 0,
-          width: 320,
-          height: 200,
-        }}
-      >
-        <CountdownTimer expiryTimestamp={time} />
-      </Rnd>
+  const layout = [
+    { i: "a", x: 0, y: 0, w: 1, h: 2, minW: 2, maxW: 4 },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+  ];
 
-      <Rnd
-        default={{
-          x: 0,
-          y: 0,
-          width: 320,
-          height: 200,
-        }}
-      >
+  return (
+    <GridLayout
+      className="layout"
+      layout={layout}
+      cols={12}
+      rowHeight={30}
+      width={1200}
+    >
+      <div key="a">
+        <CountdownTimer expiryTimestamp={time} />
+      </div>
+      <div key="b">
         <div id="outer-grey-div">
           <div id="inner-grey-div">
             <angular-component />
           </div>
         </div>
-      </Rnd>
-    </div>
+      </div>
+      <div key="c">c</div>
+    </GridLayout>
+
+    // <div>
+    //   <Rnd
+    //     default={{
+    //       x: 0,
+    //       y: 0,
+    //       width: 320,
+    //       height: 200,
+    //     }}
+    //   >
+    //     <CountdownTimer expiryTimestamp={time} />
+    //   </Rnd>
+
+    //   <Rnd
+    //     default={{
+    //       x: 0,
+    //       y: 0,
+    //       width: 320,
+    //       height: 200,
+    //     }}
+    //   >
+    //     <div id="outer-grey-div">
+    //       <div id="inner-grey-div">
+    //         <angular-component />
+    //       </div>
+    //     </div>
+    //   </Rnd>
+    // </div>
   );
 }
