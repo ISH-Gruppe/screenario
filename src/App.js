5cea458f3e456e6ee2afc3008455284c3a55c836
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-05 17:17:44 +0200

    feat: Test ResponsiveGridlayout (to be configured soon!)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import CountdownTimer from "./components/CountdownTimer";
 
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
-import GridLayout from "react-grid-layout";
+import {
+  Responsive,
+  WidthProvider,
+} from "react-grid-layout";
 
 import "./components/digitalerstuhlkreis/runtime-es2015.a4dadbc03350107420a4";
 import "./components/digitalerstuhlkreis/runtime-es5.a4dadbc03350107420a4";
@@ -17,25 +20,30 @@ export default function App() {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
 
+  // TODO: This needs further configuration!
+  // Not sure if we benefit from user Gridlayout ResponsiveGridLayout.
+  // The one thing we do need is a responsive width provider like "WidthProvider(Responsive)"
   const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, minW: 2, maxW: 4 },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxH: 4 },
+    { i: "timer", x: 0, y: 0, w: 1, h: 2, minW: 2, maxW: 4 },
+    { i: "stuhlkreis", x: 1, y: 0, w: 3, h: 2, minW: 4, maxH: 4 },
   ];
 
+  const ResponsiveGridLayout = WidthProvider(Responsive);
+
   return (
-    <GridLayout
+    <ResponsiveGridLayout
       className="layout"
       layout={layout}
-      cols={12}
       rowHeight={30}
-      // width={1200}
+      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+      cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
     >
-      <div key="a">
+      <div key="timer">
         <CountdownTimer expiryTimestamp={time} />
       </div>
-      <div key="b">
+      <div key="stuhlkreis">
         <digitaler-stuhlkreis />
       </div>
-    </GridLayout>
+    </ResponsiveGridLayout>
   );
 }
