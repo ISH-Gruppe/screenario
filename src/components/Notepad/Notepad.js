1bc550465846ca22bc59b057921da7145b780af0
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-05 21:56:48 +0200

    feat: Install @mui/lab to use TabContext -> Create basic tabs in Notepad
    Thank you sir @https://www.youtube.com/watch?v=mN8P--bmce8
    Tabs are kind'a difficult to grasp in MUI!

--- src/components/Notepad/Notepad.js
+++ src/components/Notepad/Notepad.js
@@ -1,16 +1,39 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
+
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
+import Grid from "@mui/material/Grid";
+// import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+
 export default function Notepad() {
-  const [value, setValue] = React.useState("");
+  const [notepads, setNotepads] = React.useState("");
+  const [value, setValue] = React.useState("0");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
-      </Grid>
-    </Grid>
+    // <Grid container spacing={2}>
+    //   <Grid item xs={12}>
+    //     <ReactQuill theme="snow" value={notepads} onChange={setNotepads} />
+    //   </Grid>
+    // </Grid>
+    <>
+      <TabContext value={value}>
+        <TabList onChange={handleChange}>
+          <Tab label="Item One" value="0" />
+          <Tab label="Item Two" value="1" />
+          <Tab label="Item Three" value="2" />
+        </TabList>
+
+        <TabPanel value="0">Item One</TabPanel>
+        <TabPanel value="1">Item Two</TabPanel>
+        <TabPanel value="2">Item Three</TabPanel>
+      </TabContext>
+    </>
   );
 }
