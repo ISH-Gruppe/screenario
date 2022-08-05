7c2c577279968f08936f89291565a6b518ac799f
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-05 21:28:31 +0200

    feat: Rename CountdownTimer to Timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,14 +1,8 @@
 import React from "react";
 import { useTimer } from "react-timer-hook";
-
-import BaseWindow from "../BaseWindow/BaseWindow";
-
 import "./Timer.css";
 
-export default function Timer({ id, title, visible, onReset, onHide }) {
-  const initialTimerValue = new Date();
-  initialTimerValue.setSeconds(initialTimerValue.getSeconds() + 600);
-
+export default function Timer({ expiryTimestamp }) {
   const {
     seconds,
     minutes,
@@ -20,42 +14,34 @@ export default function Timer({ id, title, visible, onReset, onHide }) {
     resume,
     restart,
   } = useTimer({
-    initialTimerValue,
+    expiryTimestamp,
     onExpire: () => console.warn("onExpire called"),
   });
 
   return (
-    <BaseWindow
-      id="timer"
-      title="Timer"
-      visible={true}
-      onReset={onReset}
-      onHide={onHide}
-    >
-      <div className="cd-body" style={{ textAlign: "center" }}>
-        <h1>react-timer-hook </h1>
-        <p>Timer Demo</p>
+    <div className="cd-body" style={{ textAlign: "center" }}>
+      <h1>react-timer-hook </h1>
+      <p>Timer Demo</p>
 
-        <div className="cd-display">
-          <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
-          <span>{seconds}</span>
-        </div>
-
-        <p>{isRunning ? "Running" : "Not running"}</p>
-        <button onClick={start}>Start</button>
-        <button onClick={pause}>Pause</button>
-        <button onClick={resume}>Resume</button>
-        <button
-          onClick={() => {
-            // Restarts to 5 minutes timer
-            const time = new Date();
-            time.setSeconds(time.getSeconds() + 300);
-            restart(time);
-          }}
-        >
-          Restart
-        </button>
+      <div className="cd-display">
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
+        <span>{seconds}</span>
       </div>
-    </BaseWindow>
+
+      <p>{isRunning ? "Running" : "Not running"}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={resume}>Resume</button>
+      <button
+        onClick={() => {
+          // Restarts to 5 minutes timer
+          const time = new Date();
+          time.setSeconds(time.getSeconds() + 300);
+          restart(time);
+        }}
+      >
+        Restart
+      </button>
+    </div>
   );
 }
