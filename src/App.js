e38f24eec78e9a5c7396849f0de9bc6ddaa0059b
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-05 19:51:55 +0200

    feat: Create basic QrcodeGenerator - functionality ✔️ design ❌

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import "./App.css";
+// Components
 import CountdownTimer from "./components/CountdownTimer";
-
-import "/node_modules/react-grid-layout/css/styles.css";
-import "/node_modules/react-resizable/css/styles.css";
-import { Responsive, WidthProvider } from "react-grid-layout";
+import QrcodeGenerator from "./components/QrcodeGenerator/QrcodeGenerator";
 
 import "./components/digitalerstuhlkreis/runtime-es2015.a4dadbc03350107420a4";
 import "./components/digitalerstuhlkreis/runtime-es5.a4dadbc03350107420a4";
@@ -13,6 +10,14 @@ import "./components/digitalerstuhlkreis/main-es2015.d43e95d8420d8bb39d2e";
 import "./components/digitalerstuhlkreis/main-es5.d43e95d8420d8bb39d2e";
 import "./components/digitalerstuhlkreis/styles.00f6a897faa5361a9828.css";
 
+// 3rd party dependencies
+import "/node_modules/react-grid-layout/css/styles.css";
+import "/node_modules/react-resizable/css/styles.css";
+import { Responsive, WidthProvider } from "react-grid-layout";
+
+// CSS
+import "./App.css";
+
 export default function App() {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
@@ -21,7 +26,13 @@ export default function App() {
   // Not sure if we benefit from user Gridlayout ResponsiveGridLayout.
   // The one thing we do need is a responsive width provider like "WidthProvider(Responsive)"
   const layout = [
+    { i: "work-phase", x: 1, y: 0, w: 3, h: 2, minW: 4, maxH: 4 },
     { i: "timer", x: 0, y: 0, w: 1, h: 2, minW: 2, maxW: 4 },
+    { i: "random-generator", x: 1, y: 0, w: 3, h: 2, minW: 4, maxH: 4 },
+    { i: "notepad", x: 1, y: 0, w: 3, h: 2, minW: 4, maxH: 4 },
+    { i: "whiteboard", x: 1, y: 0, w: 3, h: 2, minW: 4, maxH: 4 },
+    { i: "soundboard", x: 1, y: 0, w: 3, h: 2, minW: 4, maxH: 4 },
+    { i: "qrcode-generator", x: 1, y: 0, w: 3, h: 2, minW: 4, maxH: 4 },
     { i: "stuhlkreis", x: 1, y: 0, w: 3, h: 2, minW: 4, maxH: 4 },
   ];
 
@@ -37,6 +48,9 @@ export default function App() {
     >
       <div key="timer">
         <CountdownTimer expiryTimestamp={time} />
+      </div>
+      <div key="qrcode-generator">
+        <QrcodeGenerator />
       </div>
       <div key="stuhlkreis">
         <digitaler-stuhlkreis />
---
+++ src/components/QrcodeGenerator/QrcodeGenerator.js
@@ -0,0 +1,32 @@
+import React from "react";
+import QRCode from "qrcode-react";
+import TextField from "@mui/material/TextField";
+import Grid from '@mui/material/Grid';
+
+export default function CountdownTimer() {
+  const [qrcodeValue, setQrcodeValue] = React.useState("https://ish-gruppe.de");
+
+  function handleTextChange(event) {
+    setQrcodeValue(event.target.value);
+  }
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <TextField
+          id="filled-basic"
+          type="text"
+          placeholder="Hier Text eingeben"
+          label="Eingabe"
+          variant="filled"
+          value={qrcodeValue}
+          onChange={handleTextChange}
+        />
+      </Grid>
+      
+      <Grid item xs={12}>
+        <QRCode value={qrcodeValue} />
+      </Grid>
+    </Grid>
+  );
+}
