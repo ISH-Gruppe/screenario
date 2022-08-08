4d5e94d638e9fc81e1994a9e32cf62f792547e88
liam-k <liamk@memeware.net>
2022-08-08 23:55:40 +0000

    feat: add WindowManager & ExampleNotepad components (with global state).

--- src/components/ExampleNotepad/ExampleNotepad.js
+++ src/components/ExampleNotepad/ExampleNotepad.js
@@ -2,14 +2,33 @@ import React from "react";
 
 import BaseWindow from "../BaseWindow/BaseWindow";
 
-function reset() {
-  // empty notepad
-}
+export default function ExampleNotepad({
+  content,
+  id,
+  visible,
+  onHide,
+  onChange,
+}) {
+  function handleChange(event) {
+    onChange(id, { text: event.target.value });
+  }
+  function handleReset() {
+    // reset notepad
+  }
 
-export default function WelcomeDialog() {
+  function handleHide() {
+    onHide(id);
+  }
+
   return (
-    <BaseWindow title="notepad" reset={reset}>
-      <input type="textarea" />
+    <BaseWindow
+      id="example-notepad"
+      title="Notepad"
+      visible={visible}
+      onReset={handleReset}
+      onHide={onHide}
+    >
+      <input type="textarea" value={content.text} onChange={handleChange} />
     </BaseWindow>
   );
 }
--- src/components/WindowManager/WindowManager.js
+++ src/components/WindowManager/WindowManager.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import CountdownTimer from "../CountdownTimer";
 import QrcodeGenerator from "../QrcodeGenerator/QrcodeGenerator";
 import ExampleNotepad from "../ExampleNotepad/ExampleNotepad";
@@ -16,8 +18,45 @@ import "/node_modules/react-resizable/css/styles.css";
 import { Responsive, WidthProvider } from "react-grid-layout";
 
 export default function WindowManager(props) {
+  const defaultState = {
+    "work-phase": { visible: false, content: {} },
+    "example-notepad": { visible: true, content: { text: "Test" } },
+    "timer": { visible: false, content: {} },
+    "random-generator": { visible: false, content: {} },
+    "notepad": { visible: false, content: {} },
+    "whiteboard": { visible: false, content: {} },
+    "soundboard": { visible: false, content: {} },
+    "qrcode-generator": { visible: false, content: {} },
+    "stuhlkreis": { visible: false, content: {} },
+  };
+
+  const [windows, setWindows] = useState(defaultState);
+
+  function handleChange(windowId, content) {
+    const currentVisibility = windows[windowId].visible;
+    setWindows({
+      ...windows,
+      [windowId]: { visible: currentVisibility, content: content },
+    });
+  }
+
+  function handleReset(windowId) {
+    handleChange(windowId, defaultState[windowId].content);
+  }
+
+  function handleHide(windowId) {
+    setWindows({
+      ...windows,
+      [windowId]: { visible: false, content: windows[windowId].content },
+    });
+
+    // we might need to add "content" here too
+    console.log(windowId + " hidden");
+  }
+
   const time = new Date();
   time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+
   // TODO: This needs further configuration!
   // Not sure if we benefit from user Gridlayout ResponsiveGridLayout.
   // The one thing we do need is a responsive width provider like "WidthProvider(Responsive)"
@@ -52,7 +91,14 @@ export default function WindowManager(props) {
         <digitaler-stuhlkreis />
       </div>
       <div key="example-notepad">
-        <ExampleNotepad />
+        <ExampleNotepad
+          id="example-notepad"
+          visible={windows["example-notepad"].visible}
+          content={windows["example-notepad"].content}
+          onReset={handleReset}
+          onHide={handleHide}
+          onChange={handleChange}
+        />
       </div>
     </ResponsiveGridLayout>
   );
