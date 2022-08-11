63e4684e26c99407f8b03fbf0dea045d54d17557
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-11 12:16:11 +0200

    style: Responsify Stuhlkreis

--- src/components/DigitalerStuhlkreisWrapper/DigitalerStuhlkreisWrapper.js
+++ src/components/DigitalerStuhlkreisWrapper/DigitalerStuhlkreisWrapper.js
@@ -4,10 +4,9 @@ import "./digitalerstuhlkreis/runtime-es2015.a4dadbc03350107420a4";
 import "./digitalerstuhlkreis/runtime-es5.a4dadbc03350107420a4";
 import "./digitalerstuhlkreis/polyfills-es2015.d9c8f3d13d7372708bac";
 import "./digitalerstuhlkreis/polyfills-es5.dab0688ff17f3e0c4dfd";
-import "./digitalerstuhlkreis/main-es2015.9bc9bd04922037637a39";
-import "./digitalerstuhlkreis/main-es5.9bc9bd04922037637a39";
+import "./digitalerstuhlkreis/main-es2015.a53c96b2fb21b26a5317";
+import "./digitalerstuhlkreis/main-es5.a53c96b2fb21b26a5317";
 import "./digitalerstuhlkreis/styles.31e4ffa463cb25524212.css";
-import "./DigitalerStuhlkreisWrapper.scss";
 
 /*
  * This component is an Angular project that is provided as a WebComponent
