8884bfe4c64c3765a36a1840cf10bf6be8b5946a
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-11 13:48:53 +0200

    style: Responsify Stuhlkreis font

--- src/components/DigitalerStuhlkreisWrapper/DigitalerStuhlkreisWrapper.js
+++ src/components/DigitalerStuhlkreisWrapper/DigitalerStuhlkreisWrapper.js
@@ -4,19 +4,24 @@ import "./digitalerstuhlkreis/runtime-es2015.a4dadbc03350107420a4";
 import "./digitalerstuhlkreis/runtime-es5.a4dadbc03350107420a4";
 import "./digitalerstuhlkreis/polyfills-es2015.d9c8f3d13d7372708bac";
 import "./digitalerstuhlkreis/polyfills-es5.dab0688ff17f3e0c4dfd";
-import "./digitalerstuhlkreis/main-es2015.a53c96b2fb21b26a5317";
-import "./digitalerstuhlkreis/main-es5.a53c96b2fb21b26a5317";
+import "./digitalerstuhlkreis/main-es2015.9bc9bd04922037637a39";
+import "./digitalerstuhlkreis/main-es5.9bc9bd04922037637a39";
 import "./digitalerstuhlkreis/styles.31e4ffa463cb25524212.css";
+import "./DigitalerStuhlkreisWrapper.scss";
 
-/* 
+/*
  * This component is an Angular project that is provided as a WebComponent
- * We do have to import a bunch of files that are not bundled as a single file 
+ * We do have to import a bunch of files that are not bundled as a single file
  * and whose name is thus subject to change, because of new hash values.
- * 
+ *
  * This wrapper is a mere convenience function to remove the bunch of imports from the WindowManager.
- * 
+ *
  * You can find the original project at https://digitaler-stuhlkreis.de
-*/
+ */
 export default function DigitalerStuhlkreisWrapper() {
-  return <digitaler-stuhlkreis />;
+  return (
+    <div id="DigitalerStuhlkreisWrapper">
+      <digitaler-stuhlkreis />
+    </div>
+  );
 }
