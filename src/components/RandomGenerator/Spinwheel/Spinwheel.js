5013a6971656c8119eb822824016df6e4e6c7bc5
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-12 14:38:10 +0200

    feat: Test basic custom Spinwheel

--- src/components/RandomGenerator/Spinwheel/Spinwheel.js
+++ src/components/RandomGenerator/Spinwheel/Spinwheel.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./Spinwheel.scss";
 import { Wheel } from "react-custom-roulette";
 
 export default function Spinwheel() {
   const [activeTab, setActiveTab] = React.useState("0");
   const data = [
-    { option: "0", style: { backgroundColor: "green", textColor: "black" } },
-    { option: "1", style: { backgroundColor: "white" } },
-    { option: "2" },
+    { option: "0", style: { backgroundColor: "green", textColor: "white" } },
+    { option: "1", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "3", style: { backgroundColor: "blue", textColor: "white" } },
+    { option: "4", style: { backgroundColor: "pink", textColor: "white" } },
+    { option: "5", style: { backgroundColor: "orrange", textColor: "white" } },
+    { option: "6", style: { backgroundColor: "gray", textColor: "white" } },
+    { option: "7", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "8", style: { backgroundColor: "red", textColor: "white" } },
   ];
   const [isSpinning, setIsSpinning] = React.useState(false);
 
@@ -15,20 +21,19 @@ export default function Spinwheel() {
   }
 
   return (
-    <>
+    <div className="spinwheel-wrapper">
       <Wheel
         mustStartSpinning={isSpinning}
         prizeNumber={3}
         data={data}
         backgroundColors={["#3e3e3e", "#df3428"]}
         textColors={["#ffffff"]}
-
         onStopSpinning={() => {
           setIsSpinning(false);
         }}
       />
 
       <button onClick={() => setIsSpinning(true)}>SPIN</button>
-    </>
+    </div>
   );
 }
