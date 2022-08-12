872bfb523b7c43702be9335e8bbe8dd1f67b9b23
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-12 12:07:59 +0200

    feat: Install react spinwheel

--- src/components/RandomGenerator/Spinwheel/Spinwheel.js
+++ src/components/RandomGenerator/Spinwheel/Spinwheel.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Wheel } from "react-custom-roulette";
 
 export default function Spinwheel() {
   const [activeTab, setActiveTab] = React.useState("0");
+  const data = [
+    { option: "0", style: { backgroundColor: "green", textColor: "black" } },
+    { option: "1", style: { backgroundColor: "white" } },
+    { option: "2" },
+  ];
+  const [isSpinning, setIsSpinning] = React.useState(false);
 
-  return <>Spinwheel works!</>;
+  function handleSpinClick() {
+    setIsSpinning(true);
+  }
+
+  return (
+    <>
+      <Wheel
+        mustStartSpinning={isSpinning}
+        prizeNumber={3}
+        data={data}
+        backgroundColors={["#3e3e3e", "#df3428"]}
+        textColors={["#ffffff"]}
+
+        onStopSpinning={() => {
+          setIsSpinning(false);
+        }}
+      />
+
+      <button onClick={() => setIsSpinning(true)}>SPIN</button>
+    </>
+  );
 }
