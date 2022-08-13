4cb5e611de5e22fa9b7030a6ad4ebb0085a532d9
liam-k <liamk@memeware.net>
2022-08-13 21:13:08 +0000

    feat: Group WorkPhases according to their category.

--- src/components/WorkPhase/WorkPhase.js
+++ src/components/WorkPhase/WorkPhase.js
@@ -2,26 +2,51 @@ import { ImageGroup, Image } from "react-fullscreen-image";
 import BaseWindow from "../BaseWindow/BaseWindow";
 import "./WorkPhase.css";
 
-const images = [
-  "/assets/images/einzelarbeit.jpg",
-  "/assets/images/feierabend.jpg",
-  "/assets/images/grosse-pause.jpg",
-  "/assets/images/gruppenarbeit.jpg",
-  "/assets/images/kaffeepause.jpg",
-  "/assets/images/kurze-pause.jpg",
-  "/assets/images/mittagspause.jpg",
-  "/assets/images/pair.jpg",
-  "/assets/images/partnerarbeit.jpg",
-  "/assets/images/share.jpg",
-  "/assets/images/stundenende.jpg",
-  "/assets/images/think.jpg",
-];
-
 export default function WorkPhase({ id, visible, onHide, onChange }) {
+  const workPhaseImages = {
+    "workphase": {
+      description: "Arbeitsphasen",
+      images: [
+        "/assets/images/einzelarbeit.jpg",
+        "/assets/images/partnerarbeit.jpg",
+        "/assets/images/gruppenarbeit.jpg",
+      ],
+    },
+    "break-general": {
+      description: "Pausenphasen",
+      images: [
+        "/assets/images/kaffeepause.jpg",
+        "/assets/images/mittagspause.jpg",
+        "/assets/images/feierabend.jpg",
+      ],
+    },
+    "break-school": {
+      description: "Pausenphasen (Schule)",
+      images: [
+        "/assets/images/kurze-pause.jpg",
+        "/assets/images/grosse-pause.jpg",
+        "/assets/images/stundenende.jpg",
+      ],
+    },
+    "think-pair-share": {
+      description: "Think, Pair, Share",
+      images: [
+        "/assets/images/pair.jpg",
+        "/assets/images/share.jpg",
+        "/assets/images/think.jpg",
+      ],
+    },
+    "custom-images": { description: "Eigene Bilder", images: [] },
+  };
+
   function handleReset() {}
 
   function handleHide() {
     onHide(id);
+  }
+
+  function createGallery() {
+    console.log(workPhaseImages);
   }
 
   return (
@@ -31,12 +56,45 @@ export default function WorkPhase({ id, visible, onHide, onChange }) {
       onReset={handleReset}
       onHide={handleHide}
     >
+      <h3> Arbeitsphasen </h3>
       <ImageGroup>
         <ul className="images">
-          {images.map((i) => (
+          {workPhaseImages.workphase.images.map((i) => (
             <li key={i}>
-              <Image src={i} alt="nature" />
-              <p> "Test" </p>
+              <Image src={i} />
+            </li>
+          ))}
+        </ul>
+      </ImageGroup>
+
+      <h3> Pausenphasen </h3>
+      <ImageGroup>
+        <ul className="images">
+          {workPhaseImages["break-general"].images.map((i) => (
+            <li key={i}>
+              <Image src={i} />
+            </li>
+          ))}
+        </ul>
+      </ImageGroup>
+
+      <h3> Pausenphasen (Schule) </h3>
+      <ImageGroup>
+        <ul className="images">
+          {workPhaseImages["break-school"].images.map((i) => (
+            <li key={i}>
+              <Image src={i} />
+            </li>
+          ))}
+        </ul>
+      </ImageGroup>
+
+      <h3> Think, Pair, Share </h3>
+      <ImageGroup>
+        <ul className="images">
+          {workPhaseImages["think-pair-share"].images.map((i) => (
+            <li key={i}>
+              <Image src={i} />
             </li>
           ))}
         </ul>
