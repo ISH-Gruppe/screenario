5958fc081641ccf3183e12b47db4b09125a5fd9b
liam-k <liamk@memeware.net>
2022-08-13 02:06:47 +0000

    feat: Add working whiteboard component.

--- src/components/Whiteboard/Whiteboard.js
+++ src/components/Whiteboard/Whiteboard.js
@@ -1,5 +1,18 @@
 import React from "react";
 import p5 from "p5";
+
+// UI
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import DownloadIcon from "@mui/icons-material/Download";
+
+//Icons
+import UndoIcon from "@mui/icons-material/Undo";
+import RedoIcon from "@mui/icons-material/Redo";
+
 import BaseWindow from "../BaseWindow/BaseWindow";
 
 import "./Whiteboard.css";
@@ -10,7 +23,9 @@ export default class Whiteboard extends React.Component {
     this.myRef = React.createRef();
 
     this.handleHide = this.handleHide.bind(this);
-    this.handleReset = this.handle  Reset.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+
+    this.state = { update: 0 };
   }
 
   handleReset() {
@@ -20,47 +35,129 @@ export default class Whiteboard extends React.Component {
   handleHide() {
     props.onHide(id);
   }
-  
 
   Sketch = (p) => {
-    // Native p5 functions work as they would normally but prefixed with
-    // a p5 object "p"
-    
-    const colorInput = document.getElementById('color');
-    const weight = document.getElementById('weight');
-    const clear = document.getElementById('clear');
-    const paths = [];
+    const colorInput = document.getElementById("whiteboard-color");
+    const weight = document.getElementById("whiteboard-weight");
+    const clear = document.getElementById("whiteboard-reset");
+    const download = document.getElementById("whiteboard-download");
+    const undo = document.getElementById("whiteboard-undo");
+    const redo = document.getElementById("whiteboard-redo");
+    let paths = [];
     let currentPath = [];
+    let history = [];
 
     p.setup = () => {
       let canvasWidth = document.getElementById("p5-container").clientWidth;
       let canvasHeight = document.getElementById("p5-container").clientHeight;
 
-      const renderer = p.createCanvas(canvasWidth, canvasHeight);
+      const c = p.createCanvas(800, 600);
+      p.background(255);
+      c.mousePressed(handleMousePressed);
+
+      download.addEventListener("click", () => {
+        p.saveCanvas(c, "ScreenarioSkizze", "jpg");
+      });
     };
 
     p.draw = () => {
-      // And everything that normally goes in draw in here
-      p.background(220, 141, 155);
-      p.circle(p.width / 2, p.height / 2, 50);
+      p.noFill();
+      if (p.mouseIsPressed && p.mouseX >= 0 && p.mouseY >= 0) {
+        const point = {
+          x: p.mouseX,
+          y: p.mouseY,
+          color: colorInput.value,
+          weight: weight.value,
+        };
+        currentPath.push(point);
+      }
+
+      paths.forEach((path) => {
+        p.beginShape();
+        path.forEach((point) => {
+          p.stroke(point.color);
+          p.strokeWeight(point.weight);
+          p.vertex(point.x, point.y);
+        });
+        p.endShape();
+      });
     };
+
+    function handleMousePressed() {
+      currentPath = [];
+      paths.push(currentPath);
+    }
+
+    clear.addEventListener("click", () => {
+      paths.splice(0);
+      p.background(255);
+      history = [];
+    });
+
+    undo.addEventListener("click", () => {
+      const lastPath = paths[paths.length - 1];
+      if (lastPath) {
+        p.background(255);
+        history.push(lastPath);
+        paths.pop();
+      }
+    });
+
+    redo.addEventListener("click", () => {
+      const lastPath = history[history.length - 1];
+      if (lastPath) {
+        p.background(255);
+        paths.push(lastPath);
+        history.pop();
+        console.log("redo. history & paths :");
+      }
+    });
   };
 
   componentDidMount() {
-    //We create a new p5 object on component mount, feed it
+    // create a new p5 object on component mount, feed it
     this.myP5 = new p5(this.Sketch, this.myRef.current);
   }
 
   render() {
     return (
-      //This div will contain our p5 sketch
       <BaseWindow
         id="whiteboard"
         title="Whiteboard"
         onReset={this.handleReset}
         onHide={this.handleHide}
       >
-        <div id="p5-container" ref={this.myRef}></div>
+        <div id="whiteboard-wrapper">
+          <div className="sidebar">
+            <IconButton aria-label="undo" id="whiteboard-undo" color="primary">
+              <UndoIcon />
+            </IconButton>
+            <IconButton aria-label="redo" id="whiteboard-redo" color="primary">
+              <RedoIcon />
+            </IconButton>
+
+            <label htmlFor="color">Color:</label>
+            <input type="color" id="whiteboard-color" />
+            <label htmlFor="weight">Stroke:</label>
+            <input
+              type="range"
+              id="whiteboard-weight"
+              min="2"
+              max="20"
+              defaultValue="3"
+            />
+
+            <IconButton
+              aria-label="download"
+              id="whiteboard-download"
+              color="primary"
+            >
+              <DownloadIcon />
+            </IconButton>
+          </div>
+
+          <div id="p5-container" ref={this.myRef}></div>
+        </div>
       </BaseWindow>
     );
   }
