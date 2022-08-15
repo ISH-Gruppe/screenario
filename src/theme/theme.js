edb912ceeab0561111b0067dd0514140582036a0
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-15 08:27:01 +0200

    style: Use ISH-Manufaktur primary color

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -4,8 +4,36 @@ import "./font.css";
 
 export const appTheme = createTheme({
   palette: {
-    primary: pink,
-    secondary: blue,
+    primary: {
+      main: "#ff705f",
+      light: "#ffd4cf",
+      dark: "#ff5343",
+    },
+    secondary: {
+      main: "#66ccc7",
+      // light: "#ffd4cf",
+      // dark: "#ff5343",
+    },
+    warning: {
+      main: "#ab2b2b",
+      light: "#e6bfbf",
+      dark: "#ab2b2b",
+    },
+  },
+  components: {
+    // A demonstration how styles are applied and overriden
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          // backgroundImage: "linear-gradient(to top, #d299c2 0%, #fef9d7 100%)",
+          color: "white",
+        },
+        outlined: {
+          // backgroundImage: "linear-gradient(to top, #d299c2 0%, #fef9d7 100%)",
+          // color: "green",
+        },
+      },
+    },
   },
   typography: {
     fontFamily: "Rubik",
