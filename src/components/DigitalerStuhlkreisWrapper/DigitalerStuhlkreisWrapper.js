a39d766d773a97ad58482a8d13a77c4a9345c264
Sergej Grilborzer <sergej@grilborzer.de>
2022-08-24 09:34:46 +0200

    fix: Remove hashes from Stuhlkreis files and fix usage in Safari
    Changing the import order of the Stuhlkreis files fixed #77

--- src/components/DigitalerStuhlkreisWrapper/DigitalerStuhlkreisWrapper.js
+++ src/components/DigitalerStuhlkreisWrapper/DigitalerStuhlkreisWrapper.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import Card from "@mui/material/Card";
 
-import "./digitalerstuhlkreis/runtime-es2015.a4dadbc03350107420a4";
-import "./digitalerstuhlkreis/runtime-es5.a4dadbc03350107420a4";
-import "./digitalerstuhlkreis/polyfills-es2015.d9c8f3d13d7372708bac";
-import "./digitalerstuhlkreis/polyfills-es5.dab0688ff17f3e0c4dfd";
-import "./digitalerstuhlkreis/main-es2015.4426b97beb1b7ce9561b";
-import "./digitalerstuhlkreis/main-es5.4426b97beb1b7ce9561b";
-import "./digitalerstuhlkreis/styles.0d4b2152c4844f943b6e.css";
+import "./digitalerstuhlkreis/main-es2015";
+import "./digitalerstuhlkreis/main-es5";
+import "./digitalerstuhlkreis/polyfills-es2015";
+import "./digitalerstuhlkreis/polyfills-es5";
+import "./digitalerstuhlkreis/runtime-es2015";
+import "./digitalerstuhlkreis/runtime-es5";
+import "./digitalerstuhlkreis/styles.css";
 import "./DigitalerStuhlkreisWrapper.scss";
 
 import IconButton from "@mui/material/IconButton";
