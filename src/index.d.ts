c18ed03af9925bc96921f9437e06e698bac6e47f
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-12-12 10:36:27 +0100

    chore: make SCSS modules importable

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,8 @@ declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.png";
 declare module "*.mp3";
+
+declare module "*.scss" {
+  const content: {[className: string]: string};
+  export = content;
+}
